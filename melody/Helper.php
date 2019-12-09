a3e86e5227da174d02835a603cd49e45a009baa3
caoting <307298900@qq.com>
2019-12-09 13:39:22 +0800

    log init

--- melody/Helper.php
+++ melody/Helper.php
@@ -3,3 +3,33 @@
  * 助手函数列表
  */
 
+use Melody\Register;
+
+if (!function_exists("halt")) {
+    function halt(...$args)
+    {
+        if (!empty($args)) {
+            foreach ($args as $item) {
+                var_dump($item);
+            }
+        }
+        exit;
+    }
+}
+
+if (!function_exists("setLog")) {
+    function setLog($content, $tag, $logPath = "")
+    {
+        $namespace = "";
+        $backtrace = debug_backtrace();
+        if (!empty($backtrace[0]['file'])) {
+            $root = strtolower(ROOT);
+            $filePath = strtolower($backtrace[0]['file']);
+            $namespace = str_replace($root, "", $filePath);
+        }
+
+        $log = Register::get("Melody\\Log");
+        $log->setLog($content, $tag, $namespace, $logPath);
+        return;
+    }
+}
--- melody/db/Mysql.php
+++ melody/db/Mysql.php
@@ -1,7 +1,25 @@
 <?php
 namespace Melody\Db;
 
-class Mysql
-{
+use PDO;
+use PDOException;
+use Melody\Db;
 
+class Mysql extends Db
+{
+    private $dsn;
+    private $userName = null;
+    private $passWd = null;
+    private $option = null;
+
+    public function connect()
+    {
+        try{
+            $pdo = new PDO($this->dsn, $this->userName, $this->passWd, $this->option);
+        }catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+        return $pdo;
+    }
 }
