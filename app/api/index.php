5a80e7d374fcce17be357fc3e18d8a5136537458
caoting <307298900@qq.com>
2019-12-11 11:21:46 +0800

    article init, list init

--- app/api/index.php
+++ app/api/index.php
@@ -10,24 +10,66 @@ class Index
      */
     public function index()
     {
-        return ['Code' => 200];
+        return ['Code' => 0];
     }
 
     /**
      * @return array
-     * @api http://notes.com/melody/?r=/app/api/getArticle
+     * @api http://notes.com/melody/?r=/app/api/index/getArticle
      */
     public function getArticle()
     {
-        return [];
+        $id = intval($_POST['Id']);
+        if ($id <= 0) {
+            return ['Code' => 1, 'Msg' => 'Error', 'Data' => []];
+        }
+
+        $sql = "SELECT * FROM Article WHERE Id = {$id}";
+
+        $query = dbR()->prepare($sql);
+        $query->execute();
+        $data = $query->fetch();
+        if ($data === false) {
+            return ['Code' => 404, 'Msg' => '没有找到对应的文章', 'Data' => []];
+        }
+
+        return ['Code' => 0, 'Msg' => '', 'Data' => [
+            'Title' => $data['Title'],
+            "Content" => $data['Content'],
+            'UpdateTime' => $data['UpdateTime'],
+            'CreateTime' => $data['CreateTime']
+        ]];
     }
 
     /**
      * @return array
-     * @api http://notes.com/melody/?r=/app/api/getArticleList
+     * @api http://notes.com/melody/?r=/app/api/index/getArticleList
      */
     public function getArticleList()
     {
-        return [];
+        $offset = (int)$_POST['Offset'];
+        $limit = (int)$_POST['Limit'];
+
+        $offset = $offset < 0 ? 0 : $offset;
+        $limit = $limit <= 0 ? 10 : $limit;
+
+        $sql = "SELECT Id,Title,UpdateTime FROM Article LIMIT {$limit} OFFSET {$offset}";
+        $query = dbR()->prepare($sql);
+        $query->execute();
+        $result = $query->fetchAll();
+        if (empty($result)) {
+            return ['Code' => 404, 'Msg' => '当前没有文章', 'Data' => []];
+        }
+
+        $articleList = [];
+        foreach ($result as $item) {
+            $articleList[] = [
+                'Id' => $item['Id'],
+                'Title' => $item['Title'],
+                'UpdateTime' => $item['UpdateTime']
+            ];
+        }
+
+        return ['Code' => 0, 'Msg' => '', 'Data' => ['List' => $articleList]];
     }
 }
