37409db73bd97d561375d5e762e0b68f61b83093
caoting <307298900@qq.com>
2019-12-16 10:55:48 +0800

    user init

---
+++ app/api/user.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * User接口
+ */
+
+namespace App\Api;
+
+class User
+{
+    /**
+     * User
+     * @return array
+     */
+    public function index()
+    {
+        return ['Code' => 0];
+    }
+
+    public function login()
+    {
+
+    }
+}
--- app/config.php
+++ app/config.php
@@ -1,13 +1,18 @@
 <?php
 return [
     // 使用的数据库模块, 目前只支持Mysql
-    "source_name" => "Mysql",
+    "source_name"       =>              "Mysql",
 
     // 单例数据库配置
-    "db_default" => [
+    "db_default"        =>              [
         'db_host'               =>      "127.0.0.1",
         'db_name'               =>      "melody",
         'db_username'           =>      "root",
         'db_password'           =>      "root",
     ],
+
+    // 登陆配置
+    "user_account"      =>              "caoting",
+    "user_passWd"       =>              "94525",
+    "login_hash"        =>              "6e5a17b2b461ae7f8a2b6a13ac321597",
 ];
